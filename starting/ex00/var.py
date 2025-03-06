a98f63008485090d9dbf9eb67fc1692675a22628
TonUpMan <qdevianne@gmail.com>
2025-03-06 09:23:31 +0100

    my_var ok

--- starting/ex00/var.py
+++ starting/ex00/var.py
@@ -1,24 +1,24 @@
-# 42 est de type <class 'int'>
-# 42 est de type <class 'str'>
-# quarante-deux est de type <class 'str'>
-# 42.0 est de type <class 'float'>
-# True est de type <class 'bool'>
-# [42] est de type <class 'list'>
-# {42: 42} est de type <class 'dict'>
-# (42,) est de type <class 'tuple'>
-# set() est de type <class 'set'>
-
 def my_var():
     a = 42
     A = "42"
     number = "quarante-deux"
     b = 42.0
     B = True
+    l = [42]
+    d = {42: 42}
+    t = 42,
+    s = set()
+
     print(a, "est de type", type(a))
     print(A, "est de type", type(A))
     print(number, "est de type", type(number))
     print(b, "est de type", type(b))
     print(B, "est de type", type(B))
+    print(l, "est de type", type(l))
+    print(d, "est de type", type(d))
+    print(t, "est de type", type(t))
+    print(s, "est de type", type(s))
+
 
 
 if __name__ == '__main__':
