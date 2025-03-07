2b64beda09adc96c12611925caa86520d4bb4a4d
qdeviann <qdeviann@work.myguest.virtualbox.org>
2025-03-07 18:27:53 +0100

    all_in ok

--- starting/ex05/all_in.py
+++ starting/ex05/all_in.py
@@ -1,20 +1,23 @@
 import sys
 
-def find_states(states, capital_cities):
-    if a in states:
-        return (capital_cities[states[a]])
+def find_capital(states, capital_cities, item):
+    item = item.lower()
+    for i in states:
+        if i.lower() == item:
+            return(capital_cities[states[i]])
     else:
         return("")
 
 # ##################################################################
 
-def find_capital(states, capital_cities):
+def find_states(states, capital_cities, item):
+    key = ""
     for i in capital_cities:
-        if capital_cities[i] == a:
+        if capital_cities[i].lower() == item.lower():
             key = i
     if key:
         for i in states:
-            if states[i] == key:
+            if states[i].lower() == key.lower():
                 return(i)
     return("")
 
@@ -43,11 +46,16 @@ def find_all(a):
     "CO": "Denver"
     }
     for item in li:
-        value = find_capital(states, capital_cities)
-        if not value:
-            value = find_states(states, capital_cities)
-        if not value:
-            print(item, "is neither a capital city nor a state")
+        if item:
+            capital = find_capital(states, capital_cities, item)
+            state = find_states(states, capital_cities, item)
+            if not capital and not state:
+                print(item, "is neither a capital city nor a state")
+            else:
+                if state:
+                    print(item.capitalize(), "is the capital of", find_states(states, capital_cities, item))
+                else:
+                    print(find_capital(states, capital_cities, item),  "is the capital of", item.capitalize())
 
 
 # ##################################################################
